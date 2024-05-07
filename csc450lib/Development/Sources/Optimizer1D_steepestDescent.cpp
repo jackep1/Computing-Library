22f8877f61b73e02f5b242f09baafcf3111a5d39
Jonathan Pincince <pincincejack@gmail.com>
2024-05-07 11:42:51 -0400

    steepestDescent success updates correctly

--- csc450lib/Development/Sources/Optimizer1D_steepestDescent.cpp
+++ csc450lib/Development/Sources/Optimizer1D_steepestDescent.cpp
@@ -2,21 +2,24 @@
 #include <stdlib.h>
 
 OptimizerRecord1D Optimizer1D_steepestDescent::minimize(float startX, float xMin, float xMax, float tol, int maxNumIters) const {
+
     // Initialize the record
     float optX, optVal;
     int numIters = 0;
-    bool isSuccess;
-    std::shared_ptr<const Function1D> f = this->getFunction();
+    bool isSuccess = false;
 
     // Loop until the tolerance is met or the maximum number of iterations is reached
     while (std::abs(f->func(startX)) > tol && numIters < maxNumIters) {
         // Update the record
         numIters++;
-        record.x = record.x - f->derivative({record.x});
+        optX = optX - f->dfunc({optX});
 
         // Update the function value
-        record.fx = f->func({record.x});
+        optVal = f->func({optX});
+    }
+    if (numIters < maxNumIters) {
+        isSuccess = true;
     }
 
-    return OptimizerRecord1D(xMin, xMax, numIters, isSuccess);
+    return OptimizerRecord1D(optX, optVal, numIters, isSuccess);
 }
