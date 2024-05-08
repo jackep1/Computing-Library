b9024865bbd7c1327ad1d621de07f7662dd2cd6f
Jonathan Pincince <pincincejack@gmail.com>
2024-05-08 14:15:38 -0400

    Waiting for prof meeting;

--- csc450lib/Development/Sources/MortarFunc.cpp
+++ csc450lib/Development/Sources/MortarFunc.cpp
@@ -1,8 +1,17 @@
 #include <MortarFunc.h>
 #include <vector>
 
+float h(float x, float y) {
+
+}
+
 float MortarFunc::func(const std::vector<float>& x) {
+    int t;
     std::vector<float> m_pos;
-    std::vector<float> component1 = {0.0, 0.0, 0.0};
-    component1.map([](float x) { return 0.5 * x; });
+    std::vector<float> component1 = {0.0, 0.0, -9.8};
+    std::vector<float> component2 = {this->Vx0, this->Vy0, this->Vz0};
+    std::vector<float> component3 = {this->x0, this->y0, this->h(this->x0, this->y0)};
+    for (int i = 0; i < 3; i++) {
+        component1[i] = component1[i] * 0.5 * t * t;
+    }
 }
--- csc450lib/SDK/Headers/MortarFunc.h
+++ csc450lib/SDK/Headers/MortarFunc.h
@@ -6,6 +6,13 @@
 class MortarFunc : public csc450lib_calc::FunctionND {
     public:
         float func(const std::vector<float>& x);
+    private:
+        float x0;
+        float y0;
+        float Vx0;
+        float Vy0;
+        float Vz0;
+        float h(float x, float y);
 };
 
 #endif /// MORTARFUNC_H
