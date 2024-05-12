675328cfa201caac980fb9dfca0e84d7011ede8e
Jonathan Pincince <pincincejack@gmail.com>
2024-05-12 10:59:23 -0400

    Put record classes into related header files

--- csc450lib/SDK/Headers/Optimizer1D.h
+++ csc450lib/SDK/Headers/Optimizer1D.h
@@ -1,5 +1,43 @@
-#include <OptimizerRecord1D.h>
 #include <Function1D.h>
+
+#ifndef OPTIMIZERRECORD1D_H
+#define OPTIMIZERRECORD1D_H
+
+namespace csc450lib_calc {
+
+    class OptimizerRecord1D {
+        
+        public:
+            // Copy and move constructors not needed
+            virtual ~OptimizerRecord1D() = default;
+            OptimizerRecord1D(const OptimizerRecord1D& obj) = delete;
+            OptimizerRecord1D(OptimizerRecord1D&& obj) = delete;
+            OptimizerRecord1D& operator =(const OptimizerRecord1D& obj) = delete;
+            OptimizerRecord1D& operator =(OptimizerRecord1D&& obj) = delete;
+
+            // Constructor
+            OptimizerRecord1D(float optX, float optVal, int numIters, bool isSuccess) {
+                this->optX = optX;
+                this->optVal = optVal;
+                this->numIters = numIters;
+                this->isSuccess = isSuccess;
+            }
+
+        protected:
+            // The value of x that minimizes the function
+            float optX;
+            // The value of the function at optX
+            float optVal;
+            // The number of iterations it took to minimize the function
+            int numIters;
+            // Whether the minimization was successful
+            bool isSuccess;
+    };
+}
+
+#endif // OPTIMIZERRECORD1D_H
+
+
 
 #ifndef OPTIMIZER1D_H
 #define OPTIMIZER1D_H
