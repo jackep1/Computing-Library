f5728fe7b072d274d2e508a72863607ba6f8b70e
Jonathan Pincince <pincincejack@gmail.com>
2024-05-12 17:28:29 -0400

    consolidating collision nonsense

--- csc450lib/Development/Sources/CollisionProblem.cpp
+++ csc450lib/Development/Sources/CollisionProblem.cpp
@@ -1,4 +1,9 @@
 #include <CollisionProblem.h>
+#include <string>
+
+using namespace csc450lib_calc;
+
+/** Collision Problem methods */
 
 /**
  * CollisiomProblem constructor; consists of a ballistic
@@ -21,4 +26,217 @@ float CollisionProblem::func(float t) const {
     std::vector<float> position = this->ballistic->getPosition(t);
     float surface_height = this->surface->func(position[0]);
     return position[1] - surface_height;
+}
+
+
+
+/** Ballistic Function methods */
+
+/**
+ * The constructor for the BallisticFunctions which
+ * sets the initial x and y positions and the initial
+ * x and y velocities.
+ * 
+ * @param x0 the initial x position
+ * @param y0 the initial y position
+ * @param Vx0 the initial x velocity
+ * @param Vy0 the initial y velocity
+*/
+BallisticFunction::BallisticFunction(float x0, float y0, float Vx0, float Vy0)
+: x0(x0), y0(y0), Vx0(Vx0), Vy0(Vy0) {}
+
+/**
+ * Gets the position of the object at time t.
+ * 
+ * @param t the time to get the position at
+ * @return a vector containing {x, y} of the object at time t
+*/
+std::vector<float> BallisticFunction::getPosition(float t) const {
+    float x = this->x0 + this->Vx0 * t;
+    float y = this->y0 + (this->Vy0 * t) - (0.5 * 9.8 * t * t);
+    return std::vector<float>{x, y};
+}
+
+/**
+ * Gets the position and velocity of the object at time t.
+ * 
+ * @param t the time to get the position and velocity at
+ * @return a vector containing {x, y, Vx, Vy} of the object at time t
+*/
+std::vector<float> BallisticFunction::getPositionAndVelocity(float t) const {
+    float x = this->x0 + this->Vx0 * t;
+    float y = this->y0 + (this->Vy0 * t) - (0.5 * 9.8 * t * t);
+    float Vx = this->Vx0;
+    float Vy = this->Vy0 - (9.8 * t);
+    return std::vector<float>{x, y, Vx, Vy};
+}
+
+
+
+/** Base surface methods */
+
+/**
+ * Constructs a surface with a given elasticity value.
+ * 
+ * @param alpha the elasticity value of the surface
+*/
+Surface::Surface(float alpha) : a(alpha) {}
+
+/**
+ * Gets the outgoing velocity of the object after a collision with the surface.
+ * 
+ * @param x the x position of the object
+ * @param Vinx the x velocity of the object
+ * @param Viny the y velocity of the object
+ * @return a vector containing {x, Voutx, Vouty} of the object after the collision
+*/
+std::vector<float> Surface::getOutgoingVelocity(float x, float Vinx, float Viny) const {
+    float n1 = (1 / sqrt(1 + this->dfunc(x) * this->dfunc(x))) * this->dfunc(x);
+    float n2 = (1 / sqrt(1 + this->dfunc(x) * this->dfunc(x))) * 1;
+    std::vector<float> normal_force = std::vector<float>{n1, n2};
+    std::vector<float> aN = std::vector<float>{a * normal_force[0], a * normal_force[1]};
+    std::vector<float> Vout = std::vector<float>{Vinx + aN[0], Viny + aN[1]};
+    return std::vector<float>{x, Vout[0], Vout[1]};
+}
+
+/**
+ * Sets the elasticity value of the surface.
+*/
+void Surface::setAlpha(float alpha) {
+    this->a = alpha;
+}
+
+
+
+/** FlatSurface methods */
+
+/**
+ * FlatSurface constructor; consists of a single parameter.
+ * 
+ * @param alpha the parameter
+ * @return a FlatSurface object
+*/
+FlatSurface::FlatSurface(float alpha) : Surface(alpha) {}
+
+/**
+ * Gets the height of the surface at a given x value.
+ * 
+ * @param x the x value
+ * @return the height of the surface at x
+*/
+float FlatSurface::func(float x) const {
+    return 1;
+}
+
+/**
+ * @return whether the derivative of the surface is exact
+*/
+bool FlatSurface::derivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return whether the second derivative of the surface is exact
+*/
+bool FlatSurface::secondDerivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return the expression of the surface in Mathematica format
+*/
+std::shared_ptr<std::string> FlatSurface::getExpressionMMA() const {
+    std::shared_ptr<std::string> expression(new std::string("1"));
+    return expression;
+}
+
+
+
+/** EasySurface methods */
+
+/**
+ * EasySurface constructor; consists of a single parameter.
+ * 
+ * @param alpha the parameter
+ * @return an EasySurface object
+*/
+EasySurface::EasySurface(float alpha) : Surface(alpha) {}
+
+/**
+ * Gets the height of the surface at a given x value.
+ * 
+ * @param x the x value
+ * @return the height of the surface at x
+*/
+float EasySurface::func(float x) const {
+    return (cosf(x) / 5) + 1;
+}
+
+/**
+ * @return whether the derivative of the surface is exact
+*/
+bool EasySurface::derivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return whether the second derivative of the surface is exact
+*/
+bool EasySurface::secondDerivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return the expression of the surface in Mathematica format
+*/
+std::shared_ptr<std::string> EasySurface::getExpressionMMA() const {
+    std::shared_ptr<std::string> expression(new std::string("Cos[x]/5 + 1"));
+    return expression;
+}
+
+
+
+/** HardSurface methods */
+
+/**
+ * HardSurface constructor; consists of a single parameter.
+ * 
+ * @param alpha the parameter
+ * @return a HardSurface object
+*/
+HardSurface::HardSurface(float alpha) : Surface(alpha) {}
+
+/**
+ * Gets the height of the surface at a given x value.
+ * 
+ * @param x the x value
+ * @return the height of the surface at x
+*/
+float HardSurface::func(float x) const {
+    float t1 = 4 * sinf(x);
+    float t2 = 5 * cosf(x/2);
+    float t3 = x * x * x / 10000;
+    return t1 + t2 + t3;
+}
+
+/**
+ * @return whether the derivative of the surface is exact
+*/
+bool HardSurface::derivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return whether the second derivative of the surface is exact
+*/
+bool HardSurface::secondDerivativeIsExact() const {
+    return false;
+}
+
+/**
+ * @return the expression of the surface in Mathematica format
+*/
+std::shared_ptr<std::string> HardSurface::getExpressionMMA() const {
+    std::shared_ptr<std::string> expression(new std::string("4 Sin[x] + 5 Cos[x/2] + (x^3)/10000"));
+    return expression;
 }
--- csc450lib/SDK/Headers/CollisionProblem.h
+++ csc450lib/SDK/Headers/CollisionProblem.h
@@ -1,26 +1,132 @@
-#include <BallisticFunction.h>
-#include <Surface.h>
+#include <Function1D.h>
+#include <cmath>
+#include <vector>
 
-namespace csc450lib_calc {
 
-    #ifndef COLLISIONPROBLEM_H
-    #define COLLISIONPROBLEM_H
+#ifndef COLLISIONPROBLEM_H
+#define COLLISIONPROBLEM_H
 
-    class CollisionProblem : public Function1D {
-        public:
-            // Public methods for the interface
-            CollisionProblem(BallisticFunction* ballistic, Surface* surface);
-            float func(float t) const;
+class CollisionProblem : public csc450lib_calc::Function1D {
+    public:
+        // Public methods for the interface
+        CollisionProblem(BallisticFunction* ballistic, Surface* surface);
+        float func(float t) const;
 
-            // Some methods that are useless but must be defined
-            bool derivativeIsExact() const { return false; }
-            bool secondDerivativeIsExact() const { return false; }
-            std::shared_ptr<std::string> getExpressionMMA() const { return nullptr; }
+        // Some methods that are useless but must be defined
+        bool derivativeIsExact() const { return false; }
+        bool secondDerivativeIsExact() const { return false; }
+        std::shared_ptr<std::string> getExpressionMMA() const { return nullptr; }
 
-        protected:
-            BallisticFunction* ballistic;
-            Surface* surface;
-    };
+    protected:
+        BallisticFunction* ballistic;
+        Surface* surface;
+};
 
-    #endif // COLLISIONPROBLEM_H
-}
+#endif // COLLISIONPROBLEM_H
+
+
+
+#ifndef BALLISTICFUNCTION_H
+#define BALLISTICFUNCTION_H
+
+class BallisticFunction {
+    public:
+        // Public methods for the interface
+        BallisticFunction(float x0, float y0, float Vx0, float Vy0);
+        std::vector<float> getPosition(float t) const;
+        std::vector<float> getPositionAndVelocity(float t) const;
+
+    protected:
+        // The initial x position
+        float x0;
+        // The initial y position
+        float y0;
+        // The initial horizontal velocity
+        float Vx0;
+        // The initial vertical velocity
+        float Vy0;
+};
+
+#endif // BALLISTICFUNCTION_H
+
+
+
+#ifndef SURFACE_H
+#define SURFACE_H
+
+/**
+ * Represents a surface in a collision problem.
+*/
+class Surface : public csc450lib_calc::Function1D {
+
+    public:
+        Surface(float alpha);
+        std::vector<float> getOutgoingVelocity(float x, float Vinx, float Viny) const;
+        void setAlpha(float alpha);
+
+    protected:
+        // The elasticity value of the surface
+        float a;
+};
+
+#endif // SURFACE_H
+
+
+
+#ifndef FLATSURFACE_H
+#define FLATSURFACE_H
+
+/**
+ * The FlatSurface class represents a flat surface at y = 1.
+*/
+class FlatSurface : public Surface {
+    
+    public:
+        FlatSurface(float alpha);
+        float func(float x) const;
+        bool derivativeIsExact() const;
+        bool secondDerivativeIsExact() const;
+        std::shared_ptr<std::string> getExpressionMMA() const;
+};
+
+#endif // FLATSURFACE_H
+
+
+
+#ifndef EASYSURFACE_H
+#define EASYSURFACE_H
+
+/**
+ * The EasySurface class represents a surface defined
+ * by the function f(x) = cos(x)/5 + 1.
+*/
+class EasySurface : public Surface {
+    public:
+        EasySurface(float alpha);
+        float func(float x) const;
+        bool derivativeIsExact() const;
+        bool secondDerivativeIsExact() const;
+        std::shared_ptr<std::string> getExpressionMMA() const;
+};
+
+#endif // EASYSURFACE_H
+
+
+
+#ifndef HARDSURFACE_H
+#define HARDSURFACE_H
+
+/**
+ * The HardSurface class represents a surface defined
+ * by the function f(x) = 4sin(x) + 5cos(x/2) + (x^3)/10000.
+*/
+class HardSurface : public Surface {
+    public:
+        HardSurface(float alpha);
+        float func(float x) const;
+        bool derivativeIsExact() const;
+        bool secondDerivativeIsExact() const;
+        std::shared_ptr<std::string> getExpressionMMA() const;
+};
+
+#endif // HARDSURFACE_H
